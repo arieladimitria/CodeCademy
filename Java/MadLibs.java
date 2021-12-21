8707122c89af88b215422035f21470aa3f8a913f
Ariela Dimitria <94972530+arieladimitria@users.noreply.github.com>
2021-12-21 17:24:47 -0300

    Create MadLibs.java
    In this project, we’ll use Java to write a Mad Libs word game! Mad Libs have short stories with blank spaces that a player can fill in. The result is usually funny (or strange).
    
    Mad Libs require:
    
    A short story with blank spaces (asking for different types of words).
    Words from the player to fill in those blanks.

---
+++ Java/MadLibs.java
@@ -0,0 +1,30 @@
+public class MadLibs {
+  /*
+  Programa desenvolvido com o Codecademy
+  Autora: Ariela
+  Data: 21/12/2021
+  */
+  	public static void main(String[] args){
+      
+      
+      //The template for the story
+      String name1 = "Bella";
+      String adjective1 = "cute";
+      String adjective2 = "funny";
+      String adjective3 = "nice";
+      String verb1 = "play";
+      String noun1 = "car";
+      String noun2 = "fire";
+      String noun3 = "church";
+      String noun4 = "Tesla";
+      String noun5 = "movie";
+      String noun6 = "coffee";
+      String name2 = "Fera";
+      int number = 9;
+      String place1 = "Tokyo";
+
+ String story = "This morning  " +name1+" woke up feeling "+ adjective1 +". 'It is going to be a  "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep  "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+      System.out.println(story);
+    }       
+}
